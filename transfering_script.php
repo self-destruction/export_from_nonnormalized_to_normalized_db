5c4b5644991224201a32c3145417bde9f52436b2
self-destruction <syrchikov_max@mail.ru>
2018-09-09 18:09:00 +0500

    implemented the compilation mysql-query

--- transfering_script.php
+++ transfering_script.php
@@ -1,21 +1,46 @@
 <?php
 
-$db=new \SQLite3("sqlite_db/url_shortener", SQLITE3_OPEN_READWRITE);
+$db_sqlite =new \SQLite3("sqlite_db/url_shortener", SQLITE3_OPEN_READWRITE);
 
 $sql = 'SELECT * FROM main_table';
 
-$results = $db->query($sql);
+$query = $db_sqlite->query($sql);
 
-$rows = [];
+$results = [];
 
-while ($res = $results->fetchArray()) {
-    $row = [];
+while ($res = $query->fetchArray()) {
     foreach ($res as $key => $value) {
         if (!is_int($key)) {
-            $row[$key] = $value;
+            [$table_name, $column_name] = explode('_', $key, 2);
+            if (is_null($value)) {
+                $value = 'null';
+            } elseif (is_string($value)) {
+                $value = '\'' . $value . '\'';
+            }
+            $results[$table_name][$column_name][] = $value;
         }
     }
-    $rows[] = $row;
 }
 
-var_dump($rows);
+//var_dump($results);
+
+
+$db_mysql = \mysqli_connect('localhost', 'root', 'root', 'url_shortener');
+
+if ( mysqli_connect_errno() ) {
+    printf("Не удалось подключиться: %s\n", mysqli_connect_error());
+    exit();
+}
+
+foreach ($results as $table_name => $row) {
+    $sql = "INSERT INTO {$table_name} VALUES ";
+    $values = [];
+
+    foreach ($row as $column_name => $column_data) {
+        $values[] = '(' . implode(',', $column_data) . ')';
+    }
+
+    $sql .= implode(',', $values);
+
+    echo $sql . "\n";
+}
